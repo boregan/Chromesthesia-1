1e98dbdc914b0f9b92dc6454111846b261311a61
Ben <117424056@umail.ucc.ie>
2021-08-17 17:39:43 +0100

    added in particles

--- src/Components/Chromesthesia.js
+++ src/Components/Chromesthesia.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
@@ -10,10 +10,14 @@ import sketch from "./sketch";
 
 // Tone.js and 
 import * as Tone from "tone";
-import "./../style.css"
+import "./../style.css";
+
+// Keybind Handler
+import Hotkeys from 'react-hot-keys';
+
+///////////////////////////////////////////////////
 
 const synth = new Tone.PolySynth().toDestination();
-
 const styles = theme => ({
   root: {
     height: '100vh',
@@ -29,64 +33,76 @@ const styles = theme => ({
 class Chromesthesia extends Component {
   constructor() {
     super();
-
+    
     // Props to send to P5 Sketch
-
-    this.state = {color: null};
-    // this.randomColor = this.randomColor.bind(this);
+    this.state = {
+      color: 'rgb(0,0,0)',
+      push: 'bug.push()'
+    };
 
     // Key bind handler 
     this.handleKey = this.handleKey.bind(this);
   }
 
-  // randomColor = () => {
-  //   this.setState({color: red})
-  // };
+  // react-hot-keys
+  onKeyDown(keyName) {
+    if (keyName === "a+e") {
+      console.log("Successful")
+      // playNote("A4 + B4 + C4") and that would 
+      this.setState({color: 'rgb(0,0,0)'});
+    }
+  }
 
   playNote(note) {
     synth.triggerAttackRelease(`${note}`, "8n");
 
+    var colourForNotes = { 
+      'C4' : 'rgb(216, 43, 46)', 
+      'C#4': 'rgb(219, 99, 53)',
+      'D4' : 'rgb(231,141,55)',
+      'D#4': 'rgb(242, 203, 51)',
+      'E4' : 'rgb(246, 233, 95)',
+      'F4' : 'rgb(192, 214, 92)',
+      'F#4': 'rgb(0,159,71)',
+      'G4' : 'rgb(0,152,144)',
+      'G#4': 'rgb(42,50,127)',
+      'A4' : 'rgb(168,146,164)',
+      'A#4': 'rgb(196,44,130)',
+      'B4' : 'rgb(161,44,86)',
+      'C5' : 'rgb(216,43,46)'
+    };
+
+    this.setState({color: colourForNotes[note]});
+
     // Basic One Note Colours
-    if(note === "C4"){
-      this.setState({color: 'rgb(216,43,46)'})
-    } else if (note === "C#4") {
-      this.setState({color: 'rgb(219,99,53)'})
-    } else if (note === 'D4') {
-      this.setState({color: 'rgb(231,141,55)'})
-    } else if (note === 'D#4') {
-      this.setState({color: 'rgb(242,203,51)'})
-    } else if (note === 'E4') {
-      this.setState({color: 'rgb(246,233,95)'})
-    } else if (note === 'F4') {
-      this.setState({color: 'rgb(192,214,92)'})
-    } else if (note === 'F#4') {
-      this.setState({color: 'rgb(0,159,71)'})
-    } else if (note === 'G4') {
-      this.setState({color: 'rgb(0,152,144)'})
-    } else if (note === 'G#4') {
-      this.setState({color: 'rgb(42,50,127)'})
-    } else if (note === 'A4') {
-      this.setState({color: 'rgb(168,146,164)'})
-    } else if (note === 'A#4') {
-      this.setState({color: 'rgb(196,44,130)'})
-    } else if (note === 'B4') {
-      this.setState({color: 'rgb(161,44,86)'})
-    } else if (note === 'C5') {
-      this.setState({color: 'rgb(216,43,46)'})
-    }
-
-    // Chords Colours
-    // e.g. Cmajor chord
-    // if (note === "C4" && note === "E4" && note === "G4") {
-    // this.setState({color: 'rgb( some colour based off studies )})
-    // }
-  }
-
-  componentDidMount() {
-    document.addEventListener('keydown', this.handleKey);
-  }
-  componentWillUnmount() {
-    document.removeEventListener('keydown', this.handleKey);
+    // if(note === "C4"){
+    //   this.setState({color: 'rgb(216,43,46)'})
+    // } else if (note === "C#4") {
+    //   this.setState({color: 'rgb(219,99,53)'})
+    // } else if (note === 'D4') {
+    //   this.setState({color: 'rgb(231,141,55)'})
+    // } else if (note === 'D#4') {
+    //   this.setState({color: 'rgb(242,203,51)'})
+    // } else if (note === 'E4') {
+    //   this.setState({color: 'rgb(246,233,95)'})
+    // } else if (note === 'F4') {
+    //   this.setState({color: 'rgb(192,214,92)'})
+    // } else if (note === 'F#4') {
+    //   this.setState({color: 'rgb(0,159,71)'})
+    // } else if (note === 'G4') {
+    //   this.setState({color: 'rgb(0,152,144)'})
+    // } else if (note === 'G#4') {
+    //   this.setState({color: 'rgb(42,50,127)'})
+    // } else if (note === 'A4') {
+    //   this.setState({color: 'rgb(168,146,164)'})
+    // } else if (note === 'A#4') {
+    //   this.setState({color: 'rgb(196,44,130)'})
+    // } else if (note === 'B4') {
+    //   this.setState({color: 'rgb(161,44,86)'})
+    // } else if (note === 'C5') {
+    //   this.setState({color: 'rgb(216,43,46)'})
+    //   // Chords Colours
+    // }  
   }
 
   handleKey = (e) => {
@@ -96,7 +112,7 @@ class Chromesthesia extends Component {
       this.playNote("C#4");
     } else if (e.keyCode === 83 ) {
       this.playNote("D4");
-    } else if (e.keyCo4de === 69 ) {
+    } else if (e.keyCode === 69 ) {
       this.playNote("D#4");
     } else if (e.keyCode === 68 ) {
       this.playNote("E4");
@@ -112,23 +128,34 @@ class Chromesthesia extends Component {
       this.playNote("A4");
     } else if (e.keyCode === 85 ) {
       this.playNote("A#4");
-    } else if (e.keyCode === 74 ) {
+    } else if (e.keyCode === 74 ) { //Single Notes
       this.playNote("B4");
     } else if (e.keyCode === 75 ) {
       this.playNote("C5");
     }
   }
 
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKey);
+  }
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKey);
+  }
+
   render() {
     const { classes } = this.props;
+  
     return (
+    <Hotkeys
+      keyName="a+e"
+      onKeyDown={this.onKeyDown.bind(this)}
+    >
     <Grid container component="main" className={classes.root}>
       <CssBaseline />
       
       <Grid item xs={12} sm={12} md={12} component={Paper} elevation={6} square>
         <div className={classes.paper}>
-          
-          <P5Wrapper sketch={sketch} color={this.state.color}/>       
+          <P5Wrapper sketch={sketch} color={this.state.color} output={this.state.output}/>       
         </div>
       </Grid>
 
@@ -172,9 +199,8 @@ class Chromesthesia extends Component {
           </button>
         </div>
       </Grid>
-
-
     </Grid>
+    </Hotkeys>
     );
   }
 }
--- src/Components/sketch.js
+++ src/Components/sketch.js
@@ -1,30 +1,36 @@
 export default function sketch (p) {
-
 	let canvas;
+	let particles = [];
 
 	p.setup = () => {
 		canvas = p.createCanvas(p.windowWidth-500, p.windowHeight - 350);
 		p.noStroke();
-	}
+		p.frameRate(60);
+
+		// Particles
+		for(let i = 0; i<p.width/10;i++){
+			particles.push(new Particle())
+		}
+	};
 
 	p.windowResized = () => {
 		p.resizeCanvas(p.windowWidth -500, p.windowHeight - 350);
-	}
+	};
 
 	p.draw = () => {
 		// NOTE: Do not use setState in the draw function or in functions that are executed
 		// in the draw function...
 		// please use normal variables or class properties for these purposes
 
+		///////////////////////////////////////
 		// DRAWING ONE 
-		p.push();
-
 		var sc = 128 + p.sin(p.frameCount/50) * 128;
-		var x = p.width/2 + p.cos(p.frameCount/30) * 120;
+		var x = p.width/2 + p.cos(p.frameCount/40) * 120;
 		var y = p.height/2 + p.sin(p.frameCount/30) * 120;
 
 		p.rect(0,0, p.width, p.height);
 		p.stroke(sc, 100);
+		p.push();
 
 		for (var i = 0; i < p.width; i += 10) {
 			p.line(i, 0, x, y);
@@ -42,8 +48,55 @@ export default function sketch (p) {
 		///////////////////////////////////////
 		
 		// DRAWING TWO
+		for(let i = 0; i < particles.length; i++) {
+			particles[i].createParticle();
+			particles[i].moveParticle();
+			particles[i].joinParticles(particles.slice(i));
+		}
 		
 	};
+
+	// this class describes the properties of a single particle.
+	class Particle {
+		// setting the co-ordinates, radius and the
+		// speed of a particle in both the co-ordinates axes.
+		constructor(){
+			this.x = p.random(0,p.width);
+			this.y = p.random(0,p.height);
+			this.r = p.random(1,8);
+			this.xSpeed = p.random(-2,2);
+			this.ySpeed = p.random(-1,1.5);
+		}
+		
+		// creation of a particle.
+		createParticle() {
+			p.noStroke();
+			p.fill('rgba(200,169,169,0.08)');
+			p.circle(this.x,this.y,this.r);
+		}
+		
+		// setting the particle in motion.
+		moveParticle() {
+			if(this.x < 0 || this.x > p.width)
+			this.xSpeed*=-1;
+			if(this.y < 0 || this.y > p.height)
+			this.ySpeed*=-1;
+			this.x+=this.xSpeed;
+			this.y+=this.ySpeed;
+		}
+		
+		// this function creates the connections(lines)
+		// between particles which are less than a certain distance apart
+		joinParticles(particles) {
+			particles.forEach(element =>{
+			let dis = p.dist(this.x,this.y,element.x,element.y);
+			if(dis<85) {
+				p.stroke('rgba(255,255,255,0.04)');
+				p.line(this.x,this.y,element.x,element.y);
+			}
+			});
+		}
+		}
 
     p.myCustomRedrawAccordingToNewPropsHandler = (newProps) => {
 		if(canvas) //Make sure the canvas has been created
