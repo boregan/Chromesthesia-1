5fbcd4d09ecdf38d9e06a7f546025558cec04093
Ben <117424056@umail.ucc.ie>
2021-09-06 15:40:58 +0100

    map colour picker to notes

--- src/Components/Chromesthesia.js
+++ src/Components/Chromesthesia.js
@@ -9,10 +9,15 @@ import P5Wrapper from 'react-p5-wrapper';
 import sketch from "./sketch";
 import sketch2 from "./sketch2";
 
-// Tone.js and 
+// Tone.js
 import * as Tone from "tone";
+
+// Colour Picker 
+import { ColorPicker } from 'material-ui-color';
 import "./../style.css";
 import "./style.css";
+
+import stringifyObject from 'stringify-object';
 
 ///////////////////////////////////////////////////
 
@@ -27,7 +32,13 @@ const styles = theme => ({
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
-  }
+  },
+  color: {
+    width: '36px',
+    height: '14px',
+    borderRadius: '2px',
+    background: 'rgb(0,0,0)',
+  },
 });
 
 class Chromesthesia extends Component {
@@ -35,57 +46,45 @@ class Chromesthesia extends Component {
     super();
     // Props to send to P5 Sketch
     this.state = {
-      color: 'rgb(0,0,0)',
+      color: '#D82B2E', // for testing
+      fill: '#D82B2E',
       sketch: sketch,
-      rotation: 160
+      
+      // Default Colours
+      c : '#D82B2E',
+      db: '#DB6335',
+      d : '#E78D37',
+      eb: '#F2CB33',
+      e : '#F6E95F',
+      f : '#C0D65C',
+      gb: '#009f47',
+      g : '#009890',
+      ab: '#2a327f',
+      a : '#a892a4',
+      bb: '#c42c82',
+      b : '#a12c56'
     };
-
     // Key bind handler 
     this.handleKey = this.handleKey.bind(this);
   }
 
-  // react-hot-keys
-  // onKeyDown(keyName) {
-  //   if (keyName === "a+e") {
-  //     console.log("Successful")
-  //     // this.setState({color})
-  //   }
-  // }
-
   playNote(note) {
-    // synth.voices.forEach( e => {
-    //   e.envelope.attack = 0.01;
-    //   e.envelope.decay = 1;
-    //   e.envelope.sustain = 1;
-    //   e.envelope.release = 0.5;
-    //   e.oscillator.type = 'triangle'
-    // })
-
     synth.triggerAttackRelease(`${note}`, "8n");
-
     var colourForNotes = { 
-      'C4' : 'rgb(216, 43, 46)', 
-      'C#4': 'rgb(219, 99, 53)',
-      'D4' : 'rgb(231,141,55)',
-      'D#4': 'rgb(242, 203, 51)',
-      'E4' : 'rgb(246, 233, 95)',
-      'F4' : 'rgb(192, 214, 92)',
-      'F#4': 'rgb(0,159,71)',
-      'G4' : 'rgb(0,152,144)',
-      'G#4': 'rgb(42,50,127)',
-      'A4' : 'rgb(168,146,164)',
-      'A#4': 'rgb(196,44,130)',
-      'B4' : 'rgb(161,44,86)',
-      'C5' : 'rgb(216,43,46)'
+      'C4' :  this.state.c, 
+      'C#4':  this.state.db,
+      'D4' :  this.state.d,
+      'D#4':  this.state.eb,
+      'E4' :  this.state.e,
+      'F4' :  this.state.f,
+      'F#4':  this.state.gb,
+      'G4' :  this.state.g,
+      'G#4':  this.state.ab,
+      'A4' :  this.state.a,
+      'A#4':  this.state.bb,
+      'B4' :  this.state.b,
     };
-
-    this.setState({color: colourForNotes[note]});
-
-    // Create a slider that uses setState
-    // lerpColor(c1, c2, amt) to combine two colours 
-    
-    
-    // Chords Colours
+    this.setState({fill: colourForNotes[note]});
   }
 
   handleKey = (e) => {
@@ -113,9 +112,7 @@ class Chromesthesia extends Component {
       this.playNote("A#4");
     } else if (e.keyCode === 74 ) {
       this.playNote("B4");
-    } else if (e.keyCode === 75 ) {
-      this.playNote("C5");
-    } 
+    }
   }
 
   componentDidMount() {
@@ -125,16 +122,58 @@ class Chromesthesia extends Component {
     document.removeEventListener('keydown', this.handleKey);
   }
 
+  // make it have two arguments, 2nd one is idenfitier 
+  // if (identifier) then this.setState({name : ''})
+
+  handleChange = (value, key) => {
+    // stringify-object to remove double quotes 
+    var hex = stringifyObject(value.hex, {
+      doubleQuotes: false,
+    });
+    hex = hex.replace(/\'/gi,'');
+    console.log(hex, key);
+
+    this.setState({fill: '#' + [hex]});
+
+    if(key === "C") {
+      this.setState({c: '#' + [hex]});
+    } else if (key === "C#") {
+      this.setState({db: '#' + [hex]});
+    } else if (key === "D") {
+      this.setState({d: '#' + [hex]});
+    } else if (key === "D#") {
+      this.setState({eb: '#' + [hex]});
+    } else if (key === "E") {
+      this.setState({e: '#' + [hex]});
+    } else if (key === "F") {
+      this.setState({f: '#' + [hex]});
+    } else if (key === "F#") {
+      this.setState({gb: '#' + [hex]});
+    } else if (key === "G") {
+      this.setState({g: '#' + [hex]});
+    } else if (key === "G#") {
+      this.setState({ab: '#' + [hex]});
+    } else if (key === "A") {
+      this.setState({a: '#' + [hex]});
+    } else if (key === "A#") {
+      this.setState({bb: '#' + [hex]});
+    } else if (key === "B") {
+      this.setState({b: '#' + [hex]});
+    } 
+  };
+
   render() {
     const { classes } = this.props;
   
     return (
     <Grid container component="main" className={classes.root}>
       <CssBaseline />
-      
       <Grid item xs={12} sm={12} md={12} component={Paper} elevation={6} square>
         <div className={classes.paper}>
-          <P5Wrapper sketch={this.state.sketch} color={this.state.color}/>  
+          <P5Wrapper 
+            sketch ={this.state.sketch} 
+            fill={this.state.fill}
+          />  
           <button
             onClick={() =>
               this.setState({
@@ -148,44 +187,153 @@ class Chromesthesia extends Component {
           </button>     
         </div>
       </Grid>
-
       <Grid item xs={12} sm={12} md={12}>
       <div className="note-wrapper">
           <button className="note white c1" onClick={() => this.playNote("C4")}>
-            C
+            <div className="picker">
+              <ColorPicker 
+                value={this.state.c} 
+                onChange={(e) => this.handleChange(e, "C")}
+                deferred 
+                disableTextfield 
+                disableAlpha
+                name="C"
+              />
+            </div>
           </button>
           <button className="note black" onClick={() => this.playNote("C#4")}>
-            C#
+            <div className="picker-black">
+              <ColorPicker 
+                value={this.state.db} 
+                onChange={(e) => this.handleChange(e, "C#")}
+                deferred 
+                disableTextfield 
+                disableAlpha
+                name="C#4"
+              />
+              </div>
           </button>
+
           <button className="note white d" onClick={() => this.playNote("D4")}>
-            D
+            <div className="picker">
+              <ColorPicker 
+                value={this.state.d} 
+                onChange={(e) => this.handleChange(e, "D")}
+                deferred 
+                disableTextfield 
+                disableAlpha
+              />
+            </div>
           </button>
+
           <button className="note black" onClick={() => this.playNote("D#4")}>
-            D#
+          <div className="picker-black">
+              <ColorPicker 
+                className="picker" 
+                value={this.state.eb} 
+                onChange={(e) => this.handleChange(e, "D#")}
+                deferred 
+                disableTextfield 
+                disableAlpha
+              />
+            </div>
           </button>
+
           <button className="note white e" onClick={() => this.playNote("E4")}>
-            E
+          <div className="picker">
+              <ColorPicker 
+                className="picker" 
+                value={this.state.e} 
+                onChange={(e) => this.handleChange(e, "E")}
+                deferred 
+                disableTextfield 
+                disableAlpha
+              />
+            </div>
           </button>
+
           <button className="note white f" onClick={() => this.playNote("F4")}>
-            F
+          <div className="picker">
+              <ColorPicker 
+                value={this.state.f} 
+                onChange={(e) => this.handleChange(e, "F")}
+                deferred 
+                disableTextfield 
+                disableAlpha
+              />
+            </div>
           </button>
+
           <button className="note black" onClick={() => this.playNote("F#4")}>
-            F#
+            <div className="picker-black">
+              <ColorPicker 
+                value={this.state.gb} 
+                onChange={(e) => this.handleChange(e, "F#")}
+                deferred 
+                disableTextfield 
+                disableAlpha
+              />
+            </div>
           </button>
+
           <button className="note white g" onClick={() => this.playNote("G4")}>
-            G
+            <div className="picker">
+                <ColorPicker 
+                  value={this.state.g} 
+                  onChange={(e) => this.handleChange(e, "G")}
+                  deferred 
+                  disableTextfield 
+                  disableAlpha
+                />
+            </div>
           </button>
+
           <button className="note black" onClick={() => this.playNote("G#4")}>
-            G#
+            <div className="picker-black">
+              <ColorPicker 
+                value={this.state.ab} 
+                onChange={(e) => this.handleChange(e, "G#")}
+                deferred 
+                disableTextfield 
+                disableAlpha
+              />
+            </div>
           </button>
+
           <button className="note white a" onClick={() => this.playNote("A4")}>
-            A
+            <div className="picker">
+              <ColorPicker 
+                value={this.state.a} 
+                onChange={(e) => this.handleChange(e, "A")}
+                deferred 
+                disableTextfield 
+                disableAlpha
+              />
+            </div>
           </button>
+
           <button className="note black" onClick={() => this.playNote("A#4")}>
-            A#
+            <div className="picker-black">
+              <ColorPicker 
+                value={this.state.bb} 
+                onChange={(e) => this.handleChange(e, "A#")}
+                deferred 
+                disableTextfield 
+                disableAlpha
+              />
+            </div>
           </button>
-          <button className="note white c" onClick={() => this.playNote("C5")}>
-            C
+
+          <button className="note white b" onClick={() => this.playNote("B4")}>
+            <div className="picker">
+              <ColorPicker 
+                value={this.state.b} 
+                onChange={(e) => this.handleChange(e, "B")}
+                deferred 
+                disableTextfield 
+                disableAlpha
+              />
+            </div>
           </button>
         </div>
       </Grid>
--- src/Components/sketch.js
+++ src/Components/sketch.js
@@ -2,52 +2,20 @@ import "./../style.css";
 
 export default function sketch (p) {
 	let canvas;
-	let particles = [];
-	let phase = 0;
-
-	let slider;
-	let slider2;
-	let slider3;
 
 	p.setup = () => {
 		p.remove();
 		p.loop();
-		canvas = p.createCanvas(p.windowWidth-500, p.windowHeight - 350);
+		canvas = p.createCanvas(p.windowWidth - 500, p.windowHeight - 350);
 		p.noStroke();
 		p.frameRate(60);
 		p.colorMode(p.HSB);
 		p.textSize(10);
 
-		// Particles
-		for(let i = 0; i<p.width/10;i++){
-			particles.push(new Particle())
-		}
-
-		// Sliders
-		// Hue
-		slider = p.createSlider(0, 255, 100, 5);
-		slider.position(80, 100);
-		slider.style('width', '80px');
-
-		// Saturation
-		slider2 = p.createSlider(0, 255, 100, 5);
-		slider2.position(80, 150);
-		slider2.style('width', '80px');
-
-		// Brightness
-		slider3 = p.createSlider(0, 255, 100, 5);
-		slider3.position(80, 200);
-		slider3.style('width', '80px');
-
-		// Labels
-		let label = p.createElement('p', 'hue');
-		label.position(slider.x / 2 - 30, 85);
-
-		let label2 = p.createElement('p', 'saturation')
-		label2.position(slider2.x / 2 - 30, 135)
-
-		let label3 = p.createElement('p', 'brightness')
-		label3.position(slider3.x / 2 - 30, 185)
+		// // Particles
+		// for(let i = 0; i<p.width/10;i++){
+		// 	particles.push(new Particle())
+		// }
 	};
 
 	p.windowResized = () => {
@@ -59,25 +27,14 @@ export default function sketch (p) {
 		// in the draw function...
 
 		///////////////////////////////////////
+
 		// Constants
 		// Particles
-		for(let i = 0; i < particles.length; i++) {
-			particles[i].createParticle();
-			particles[i].moveParticle();
-			particles[i].joinParticles(particles.slice(i));
-		}
-
-		// Sliders 
-		// Hue
-		var hue = slider.value();
-
-		// Saturation
-		var sat = slider2.value();
-
-		// Brightness
-		var bri = slider3.value();
-
-		p.fill(hue, sat, bri);
+		// for(let i = 0; i < particles.length; i++) {
+		// 	particles[i].createParticle();
+		// 	particles[i].moveParticle();
+		// 	particles[i].joinParticles(particles.slice(i));
+		// }
 
 		///////////////////////////////////////
 
@@ -88,8 +45,8 @@ export default function sketch (p) {
 
 		p.rect(0,0, p.width, p.height);
 		p.stroke(sc, 100);
+		
 		p.push();
-
 		for (var i = 0; i < p.width; i += 10) {
 			p.line(i, 0, x, y);
 		}
@@ -105,57 +62,12 @@ export default function sketch (p) {
 		p.pop();
 	};
 
-	function resetSketch() {
-		p.removeElements();
-		p.loop();
-	}
-
-	// this class describes the properties of a single particle.
-	class Particle {
-		// setting the co-ordinates, radius and the
-		// speed of a particle in both the co-ordinates axes.
-		constructor(){
-			this.x = p.random(0,p.width);
-			this.y = p.random(0,p.height);
-			this.r = p.random(1,8);
-			this.xSpeed = p.random(-2,2);
-			this.ySpeed = p.random(-1,1.5);
+	// Figure out how to pass multiple props in ?
+	p.myCustomRedrawAccordingToNewPropsHandler = (newProps) => {
+		//Make sure the canvas has been created 
+		console.log(newProps)
+		if (canvas) {
+			p.fill(newProps.fill)
 		}
-		
-		// creation of a particle.
-		createParticle() {
-			p.noStroke();
-			p.fill('rgba(200,169,169,0.08)');
-			p.circle(this.x,this.y,this.r);
-		}
-		
-		// setting the particle in motion.
-		moveParticle() {
-			if(this.x < 0 || this.x > p.width)
-			this.xSpeed*=-2;
-			if(this.y < 0 || this.y > p.height)
-			this.ySpeed*=-1;
-			this.x+=this.xSpeed;
-			this.y+=this.ySpeed;
-		}
-		
-		// this function creates the connections(lines)
-		// between particles which are less than a certain distance apart
-		joinParticles(particles) {
-			particles.forEach(element =>{
-			let dis = p.dist(this.x,this.y,element.x,element.y);
-			if(dis<85) {
-				// p.stroke('rgba(255,255,255,0.04)');
-				p.line(this.x,this.y,element.x,element.y);
-			}
-			});
-		}
-		}
-
-    p.myCustomRedrawAccordingToNewPropsHandler = (newProps) => {
-
-		//Make sure the canvas has been created  	
-		if(canvas) 
-			p.fill(newProps.color);		  
 	};
 }
